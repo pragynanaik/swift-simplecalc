63c304e8f95e3cb04b49f7c01989a15fb693ea9e
Pragyna Naik <pragynanaik@Pragynas-MacBook-Pro.local>
2021-04-15 20:38:28 -0700

    all tests pass

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,11 +1,103 @@
+import Foundation
+
 print("Welcome to the UW Calculator Playground")
 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    if (args[args.endIndex - 1] == "count") {
+        return args.endIndex - 1
+    } else if (args[args.endIndex - 1] == "avg"){
+        let number = args.endIndex - 1
+        if (number == 0) {
+            return 0;
+        } else {
+            var sum = 0
+            for i in 0...number - 1 {
+                sum = sum + Int(args[i])!
+            }
+            return sum/number;
+        }
+    } else if (args[args.endIndex - 1] == "fact") {
+        if (args.endIndex == 1) {
+            return 0
+        }
+        var mult = 1
+        
+        if (Int(args[0])! != 0) {
+            for i in 1...Int(args[0])! {
+                mult = mult * i
+            }
+        }
+        return mult
+    } else {
+        let first = Int(args[0])!
+        let second = Int(args[2])!
+        let operation = args[1]
+        
+        switch operation {
+        case "+":
+            return first + second
+        case "-":
+            return first - second
+        case "/":
+            return first / second
+        case "%":
+            return first % second
+        case "*":
+            return first * second
+        default:
+        return 0
+        }
+    }
 }
 
 func calculate(_ arg: String) -> Int {
-    return -1
+    let stringSplit = arg.components(separatedBy: " ")
+    let args = stringSplit
+    if (args[args.endIndex - 1] == "count") {
+        return args.endIndex - 1
+    } else if (args[args.endIndex - 1] == "avg"){
+        let number = args.endIndex - 1
+        if (number == 0) {
+            return 0;
+        } else {
+            var sum = 0
+            for i in 0...number - 1 {
+                sum = sum + Int(args[i])!
+            }
+            return sum/number;
+        }
+    } else if (args[args.endIndex - 1] == "fact") {
+        if (args.endIndex == 1) {
+            return 0
+        }
+        var mult = 1
+        
+        if (Int(args[0])! != 0) {
+            for i in 1...Int(args[0])! {
+                mult = mult * i
+            }
+        }
+        return mult
+    } else {
+        let first = Int(args[0])!
+        let second = Int(args[2])!
+        let operation = args[1]
+        
+        switch operation {
+        case "+":
+            return first + second
+        case "-":
+            return first - second
+        case "/":
+            return first / second
+        case "%":
+            return first % second
+        case "*":
+            return first * second
+        default:
+        return 0
+        }
+    }
 }
 
 // -------------------------------------------
@@ -53,7 +145,7 @@ calculate("5 fact") == 120
 
 // Implement calculate([String]) and calculate(String)
 // to handle negative numbers
-/*
+
 calculate(["2", "+", "-2"]) == 0
 calculate(["2", "-", "-2"]) == 4
 calculate(["2", "*", "-2"]) == -4
@@ -68,13 +160,57 @@ calculate("2 - -2") == 4
 calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
-*/
+
  
 // Implement calculate([String]) and calculate(String)
 // to use floating-point values
-/*
+
 func calculate(_ args: [String]) -> Double {
-    return -1.0
+    if (args[args.endIndex - 1] == "count") {
+        return Double(args.endIndex - 1)
+    } else if (args[args.endIndex - 1] == "avg"){
+        let number = args.endIndex - 1
+        if (number == 0) {
+            return 0.0;
+        } else {
+            var sum = 0.0
+            for i in 0...number - 1 {
+                sum = sum + Double(args[i])!
+            }
+            return sum/Double(number);
+        }
+    } else if (args[args.endIndex - 1] == "fact") {
+        if (args.endIndex == 1) {
+            return 0.0
+        }
+        var mult = 1.0
+        
+        if (Double(args[0])! != 0.0) {
+            for i in 1...Int(args[0])! {
+                mult = mult * Double(i)
+            }
+        }
+        return mult
+    } else {
+        let first = Double(args[0])!
+        let second = Double(args[2])!
+        let operation = args[1]
+        
+        switch operation {
+        case "+":
+            return first + second
+        case "-":
+            return first - second
+        case "/":
+            return first / second
+        case "%":
+            return first.truncatingRemainder(dividingBy: second)
+        case "*":
+            return first * second
+        default:
+            return 0.0
+        }
+    }
 }
 func calculate(_ arg: String) -> Double {
     return -1.0
@@ -87,4 +223,4 @@ calculate(["2.5", "*", "2.5"]) == 6.25
 calculate(["2.0", "/", "2.0"]) == 1.0
 calculate(["2.0", "%", "2.0"]) == 0.0
 calculate("1.0 2.0 3.0 4.0 5.0 count") == 5
-*/
+
